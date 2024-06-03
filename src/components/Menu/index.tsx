bb02b3dd9914290755e6ca6feed786880452f3f7
Katzo <61660416+Katzo-de@users.noreply.github.com>
2024-06-03 02:11:53 +0200

    Menu (example)
    I will revise this and will soon build the grid of the app with sidebar, menu and content area

--- src/components/Menu/index.tsx
+++ src/components/Menu/index.tsx
@@ -1,9 +1,33 @@
-function Menu() {
-    return (
-        <nav className="flex w-full h-16 px-4">
+import Library from "../../assets/icons/library.svg";
+import Home from "../../assets/icons/home.svg";
+import Discover from "../../assets/icons/discover.svg";
+import Search from "../../assets/icons/search.svg";
 
-        </nav>
-    );
+function Menu() {
+  return (
+    <nav className="flex w-full h-16 px-4 space-x-5">
+      <button className="hover:bg-tertiary my-2 space-x-3 flex items-center w-52  rounded-md bg-primary-foreground-dark text-secondary-foreground">
+        <img src={Library} alt="Library" />
+        <span>My Library</span>
+      </button>
+
+      <button className="hover:bg-tertiary my-2 space-x-3 flex items-center w-52  rounded-md bg-primary-foreground-dark text-secondary-foreground">
+        <img src={Home} alt="Home" />
+        <span>Home</span>
+      </button>
+
+      <button className="hover:bg-tertiary my-2 space-x-3 flex items-center w-52 rounded-md bg-primary-foreground-dark text-secondary-foreground">
+        <img height={35} width={35} src={Discover} alt="Discover" />
+        <span>Discover</span>
+      </button>
+
+      <button className="hover:bg-tertiary my-2 space-x-3 flex items-center w-52  rounded-md bg-primary-foreground-dark text-secondary-foreground">
+        <img src={Search} alt="Search" />
+      </button>
+
+
+    </nav>
+  );
 }
 
 export default Menu;
